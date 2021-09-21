01618269c4ec4b93a35bc637613db1c941766eae
Harsh Vardhan Goswami <46974223+iamharshdev@users.noreply.github.com>
2021-09-21 12:29:10 +0530

    K :)

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-from flask import Flask, jsonify
-from flask_restful import Api, Resource
-from flask_cors import CORS
 import bs4
 from bs4 import BeautifulSoup
 import requests as rq
-import random
+import json
 
-app = Flask(__name__)
-api = Api(app)
-CORS(app)
-cors = CORS(app, resources={
-    r"/*": {
-        "news": "*"
-    }})
+backend = "https://api.crichunt.in"
 
 
 def columnName(name):
-
     dict_ = dict(column_1="team",
                  column_2="play",
                  column_3="win",
@@ -30,9 +20,12 @@ def columnName(name):
 
 def points():
     r = []
-    data = rq.get("https://vivoiplpointstable.com/", headers={
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-    })
+    data = rq.get(
+        "https://vivoiplpointstable.com/",
+        headers={
+            "User-Agent":
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+        })
     soup = bs4.BeautifulSoup(data.content, "html.parser")
     for item in soup.find_all("table", class_='tablepress-id-18'):
         tbody = item.find("tbody")
@@ -41,18 +34,12 @@ def points():
             for td in tr.find_all("td"):
                 teamData[columnName(td['class'][0])] = td.text
             r.append(teamData)
-    rq.get("http://127.0.0.1:8000/api/table/drop")
-    for data in r:
-        rq.post("http://127.0.0.1:8000/api/add", data=data)
-    return True
+    rq.get(backend + "/api/table/drop")
+    for obj in r:
+        rq.post(backend + "/api/table/add",
+                data=json.dumps(obj),
+                headers={'Content-type': 'application/json'})
+    return "Hey"
 
 
-class matchPoints(Resource):
-    def get(self):
-        return jsonify({'points': points()})
-
-
-api.add_resource(matchPoints, '/schedule')
-
-if __name__ == '__main__':
-    app.run()
+print(points())
